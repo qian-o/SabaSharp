6c4e945db0b8dafe41b01727356b52053e8322e4
熙 王 <1324771795@qq.com>
2023-09-09 18:56:13 +0800

    更新打印信息

--- SabaViewer/Program.cs
+++ SabaViewer/Program.cs
@@ -9,8 +9,8 @@ internal class Program
         PmxParsing pmxParsing = PmxParsing.ParsingByFile("Resources/大喜/模型/登门喜鹊泠鸢yousa-ver2.0/泠鸢yousa登门喜鹊153cm-Apose2.1完整版(2).pmx")!;
 
         Console.WriteLine($"{pmxParsing.Header.Magic}{pmxParsing.Header.Version}");
-        Console.WriteLine($"{pmxParsing.Info.ModelName} {pmxParsing.Info.ModelNameEn}");
-        Console.WriteLine($"{pmxParsing.Info.Comment} {pmxParsing.Info.CommentEn}");
+        Console.WriteLine($"{pmxParsing.Info.ModelName} {pmxParsing.Info.Comment}");
+        Console.WriteLine($"{pmxParsing.Info.ModelNameEn} {pmxParsing.Info.CommentEn}");
         Console.WriteLine($"Vertices: {pmxParsing.Vertices.Length}");
         Console.WriteLine($"Faces: {pmxParsing.Faces.Length}");
         Console.WriteLine($"Textures: {pmxParsing.Textures.Length}");
