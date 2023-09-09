38c3fefbaf2e1a87ed5fde0933c0d4abb90c1b52
熙 王 <1324771795@qq.com>
2023-09-09 18:55:12 +0800

    wx

--- SabaViewer/Program.cs
+++ SabaViewer/Program.cs
@@ -7,7 +7,7 @@ internal class Program
         _ = args;
 
         PmxParsing pmxParsing = PmxParsing.ParsingByFile("Resources/大喜/模型/登门喜鹊泠鸢yousa-ver2.0/泠鸢yousa登门喜鹊153cm-Apose2.1完整版(2).pmx")!;
-    
+
         Console.WriteLine($"{pmxParsing.Header.Magic}{pmxParsing.Header.Version}");
         Console.WriteLine($"{pmxParsing.Info.ModelName} {pmxParsing.Info.ModelNameEn}");
         Console.WriteLine($"{pmxParsing.Info.Comment} {pmxParsing.Info.CommentEn}");
