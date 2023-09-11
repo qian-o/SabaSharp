391e0c2ccd28aeddec422ce84dbe6930fb02451c
王 熙 <1324771795@qq.com>
2023-09-11 14:44:30 +0800

    添加2d纹理

---
+++ SabaViewer/Helpers/TextureExtensions.cs
@@ -0,0 +1,94 @@
+﻿using SabaViewer.Tools;
+using Silk.NET.Maths;
+using Silk.NET.OpenGLES;
+using SkiaSharp;
+using System.Drawing;
+
+namespace SabaViewer.Helpers;
+
+public static unsafe class TextureExtensions
+{
+    public static void WriteImage(this Texture2D texture, string file)
+    {
+        using SKImage image = SKImage.FromEncodedData(file);
+
+        texture.AllocationBuffer((uint)(image.Width * image.Height * 4), out void* pboData);
+
+        image.ReadPixels(new SKImageInfo(image.Width, image.Height, SKColorType.Rgba8888), (nint)pboData, image.Width * 4, 0, 0);
+
+        texture.FlushTexture(new Vector2D<uint>((uint)image.Width, (uint)image.Height), GLEnum.Rgba, GLEnum.UnsignedByte);
+    }
+
+    public static void WriteImage(this Texture2D texture, byte* image, int width, int height)
+    {
+        texture.AllocationBuffer((uint)(width * height * 4), out void* pboData);
+
+        Span<byte> src = new(image, width * height * 4);
+        Span<byte> dst = new(pboData, width * height * 4);
+
+        src.CopyTo(dst);
+
+        texture.FlushTexture(new Vector2D<uint>((uint)width, (uint)height), GLEnum.Rgba, GLEnum.UnsignedByte);
+    }
+
+    public static void WriteFrame(this Texture2D texture, GL gl, int x, int y, int width, int height)
+    {
+        texture.AllocationBuffer((uint)(width * height * 4), out void* pboData);
+
+        gl.ReadPixels(x, y, (uint)width, (uint)height, GLEnum.Rgba, GLEnum.UnsignedByte, pboData);
+
+        texture.FlushTexture(new Vector2D<uint>((uint)width, (uint)height), GLEnum.Rgba, GLEnum.UnsignedByte);
+    }
+
+    public static void WriteLinearColor(this Texture2D texture, Color[] colors, PointF begin, PointF end)
+    {
+        texture.AllocationBuffer(1024 * 1024 * 4, out void* pboData);
+
+        using SKSurface surface = SKSurface.Create(new SKImageInfo(1024, 1024, SKColorType.Rgba8888), (nint)pboData);
+
+        using SKPaint paint = new()
+        {
+            IsAntialias = true,
+            IsDither = true,
+            FilterQuality = SKFilterQuality.High,
+            Shader = SKShader.CreateLinearGradient(new SKPoint(begin.X * 1024, begin.Y * 1024), new SKPoint(end.X * 1024, end.Y * 1024), colors.Select(c => new SKColor(c.R, c.G, c.B, c.A)).ToArray(), null, SKShaderTileMode.Repeat)
+        };
+        surface.Canvas.DrawRect(0, 0, 1024, 1024, paint);
+
+        texture.FlushTexture(new Vector2D<uint>(1024, 1024), GLEnum.Rgba, GLEnum.UnsignedByte);
+    }
+
+    public static void WriteMatrixArray(this Texture2D texture, IEnumerable<Matrix4X4<float>> matrices)
+    {
+        Matrix4X4<float>[] matrixArray = matrices.ToArray();
+
+        texture.AllocationBuffer((uint)(matrixArray.Length * 16 * sizeof(float)), out void* pboData);
+
+        Span<Matrix4X4<float>> span = new(pboData, matrixArray.Length);
+
+        matrixArray.CopyTo(span);
+
+        texture.FlushTexture(new Vector2D<uint>(4, (uint)matrixArray.Length), GLEnum.Rgba, GLEnum.Float);
+    }
+
+    public static void WriteColor(this Texture2D texture, Color color)
+    {
+        WriteColor(texture, new Vector4D<byte>(color.R, color.G, color.B, color.A));
+    }
+
+    public static void WriteColor(this Texture2D texture, Vector3D<byte> color)
+    {
+        WriteColor(texture, new Vector4D<byte>(color, 255));
+    }
+
+    public static void WriteColor(this Texture2D texture, Vector4D<byte> color)
+    {
+        texture.AllocationBuffer(4, out void* pboData);
+
+        Span<Vector4D<byte>> span = new(pboData, 1);
+
+        span[0] = color;
+
+        texture.FlushTexture(new Vector2D<uint>(1, 1), GLEnum.Rgba, GLEnum.UnsignedByte);
+    }
+}
---
+++ SabaViewer/Tools/Texture2D.cs
@@ -0,0 +1,78 @@
+﻿using Silk.NET.Maths;
+using Silk.NET.OpenGLES;
+using Silk.NET.OpenGLES.Extensions.EXT;
+
+namespace SabaViewer.Tools;
+
+public unsafe class Texture2D : IDisposable
+{
+    private readonly GL _gl;
+    private readonly GLEnum _internalformat;
+    private readonly uint _pbo;
+    private readonly uint _tex;
+
+    public Texture2D(GL gl, GLEnum internalformat, GLEnum wrapParam)
+    {
+        _gl = gl;
+        _internalformat = internalformat;
+
+        _pbo = _gl.GenBuffer();
+        _tex = _gl.GenTexture();
+
+        _gl.GetFloat((GLEnum)EXT.MaxTextureMaxAnisotropyExt, out float maxAnisotropy);
+
+        _gl.BindTexture(GLEnum.Texture2D, _tex);
+
+        _gl.TexParameter(GLEnum.Texture2D, (GLEnum)EXT.MaxTextureMaxAnisotropyExt, maxAnisotropy);
+        _gl.TexParameter(GLEnum.Texture2D, GLEnum.TextureMinFilter, (int)GLEnum.LinearMipmapLinear);
+        _gl.TexParameter(GLEnum.Texture2D, GLEnum.TextureMagFilter, (int)GLEnum.Linear);
+        _gl.TexParameter(GLEnum.Texture2D, GLEnum.TextureWrapS, (int)wrapParam);
+        _gl.TexParameter(GLEnum.Texture2D, GLEnum.TextureWrapT, (int)wrapParam);
+        _gl.TexParameter(GLEnum.Texture2D, GLEnum.TextureBaseLevel, 0);
+        _gl.TexParameter(GLEnum.Texture2D, GLEnum.TextureMaxLevel, 8);
+
+        _gl.BindTexture(GLEnum.Texture2D, 0);
+    }
+
+    public void Enable()
+    {
+        _gl.BindTexture(GLEnum.Texture2D, _tex);
+    }
+
+    public void Disable()
+    {
+        _gl.BindTexture(GLEnum.Texture2D, 0);
+    }
+
+    public void AllocationBuffer(uint pboSize, out void* pboData, bool needRead = false)
+    {
+        _gl.BindBuffer(GLEnum.PixelUnpackBuffer, _pbo);
+
+        _gl.BufferData(GLEnum.PixelUnpackBuffer, pboSize, null, GLEnum.StreamDraw);
+
+        pboData = _gl.MapBufferRange(GLEnum.PixelUnpackBuffer, 0, pboSize, needRead ? (uint)(GLEnum.MapReadBit | GLEnum.MapWriteBit) : (uint)GLEnum.MapWriteBit);
+
+        _gl.BindBuffer(GLEnum.PixelUnpackBuffer, 0);
+    }
+
+    public void FlushTexture(Vector2D<uint> size, GLEnum format, GLEnum type)
+    {
+        _gl.BindBuffer(GLEnum.PixelUnpackBuffer, _pbo);
+        _gl.BindTexture(GLEnum.Texture2D, _tex);
+
+        _gl.UnmapBuffer(GLEnum.PixelUnpackBuffer);
+        _gl.TexImage2D(GLEnum.Texture2D, 0, (int)_internalformat, size.X, size.Y, 0, format, type, null);
+        _gl.GenerateMipmap(GLEnum.Texture2D);
+
+        _gl.BindTexture(GLEnum.Texture2D, 0);
+        _gl.BindBuffer(GLEnum.PixelUnpackBuffer, 0);
+    }
+
+    public void Dispose()
+    {
+        _gl.DeleteBuffer(_pbo);
+        _gl.DeleteTexture(_tex);
+
+        GC.SuppressFinalize(this);
+    }
+}
