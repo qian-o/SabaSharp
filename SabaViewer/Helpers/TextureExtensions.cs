8b408b9114e915217b5c539be37588768a7ab568
王 熙 <1324771795@qq.com>
2023-09-12 15:29:23 +0800

    释放多余的纹理内存

--- SabaViewer/Helpers/TextureExtensions.cs
+++ SabaViewer/Helpers/TextureExtensions.cs
@@ -4,7 +4,6 @@ using Silk.NET.OpenGLES;
 using SkiaSharp;
 using StbImageSharp;
 using System.Drawing;
-using Buffer = System.Buffer;
 
 namespace SabaViewer.Helpers;
 
@@ -12,69 +11,66 @@ public static unsafe class TextureExtensions
 {
     public static void WriteImage(this Texture2D texture, string file)
     {
-        ImageResult image = ImageResult.FromStream(File.OpenRead(file), ColorComponents.RedGreenBlueAlpha);
-
-        int pboSize = image.Width * image.Height * 4;
-        texture.AllocationBuffer((uint)pboSize, out void* pboData);
+        ImageResult image = ImageResult.FromMemory(File.ReadAllBytes(file), ColorComponents.RedGreenBlueAlpha);
 
         fixed (byte* ptr = image.Data)
         {
-            Buffer.MemoryCopy(ptr, pboData, pboSize, pboSize);
+            texture.FlushTexture(ptr, new Vector2D<uint>((uint)image.Width, (uint)image.Height), GLEnum.Rgba, GLEnum.UnsignedByte, image.SourceComp == ColorComponents.RedGreenBlueAlpha);
         }
-
-        texture.FlushTexture(new Vector2D<uint>((uint)image.Width, (uint)image.Height), GLEnum.Rgba, GLEnum.UnsignedByte, image.SourceComp == ColorComponents.RedGreenBlueAlpha);
     }
 
     public static void WriteImage(this Texture2D texture, byte* image, int width, int height)
     {
-        texture.AllocationBuffer((uint)(width * height * 4), out void* pboData);
-
-        Span<byte> src = new(image, width * height * 4);
-        Span<byte> dst = new(pboData, width * height * 4);
-
-        src.CopyTo(dst);
-
-        texture.FlushTexture(new Vector2D<uint>((uint)width, (uint)height), GLEnum.Rgba, GLEnum.UnsignedByte);
+        texture.FlushTexture(image, new Vector2D<uint>((uint)width, (uint)height), GLEnum.Rgba, GLEnum.UnsignedByte);
     }
 
     public static void WriteFrame(this Texture2D texture, GL gl, int x, int y, int width, int height)
     {
-        texture.AllocationBuffer((uint)(width * height * 4), out void* pboData);
+        byte[] bytes = new byte[width * height * 4];
 
-        gl.ReadPixels(x, y, (uint)width, (uint)height, GLEnum.Rgba, GLEnum.UnsignedByte, pboData);
+        fixed (byte* ptr = bytes)
+        {
+            gl.ReadPixels(x, y, (uint)width, (uint)height, GLEnum.Rgba, GLEnum.UnsignedByte, ptr);
 
-        texture.FlushTexture(new Vector2D<uint>((uint)width, (uint)height), GLEnum.Rgba, GLEnum.UnsignedByte);
+            texture.FlushTexture(ptr, new Vector2D<uint>((uint)width, (uint)height), GLEnum.Rgba, GLEnum.UnsignedByte);
+        }
     }
 
     public static void WriteLinearColor(this Texture2D texture, Color[] colors, PointF begin, PointF end)
     {
-        texture.AllocationBuffer(1024 * 1024 * 4, out void* pboData);
+        byte[] bytes = new byte[1024 * 1024 * 4];
 
-        using SKSurface surface = SKSurface.Create(new SKImageInfo(1024, 1024, SKColorType.Rgba8888), (nint)pboData);
-
-        using SKPaint paint = new()
+        fixed (byte* ptr = bytes)
         {
-            IsAntialias = true,
-            IsDither = true,
-            FilterQuality = SKFilterQuality.High,
-            Shader = SKShader.CreateLinearGradient(new SKPoint(begin.X * 1024, begin.Y * 1024), new SKPoint(end.X * 1024, end.Y * 1024), colors.Select(c => new SKColor(c.R, c.G, c.B, c.A)).ToArray(), null, SKShaderTileMode.Repeat)
-        };
-        surface.Canvas.DrawRect(0, 0, 1024, 1024, paint);
+            using SKSurface surface = SKSurface.Create(new SKImageInfo(1024, 1024, SKColorType.Rgba8888), (nint)ptr);
 
-        texture.FlushTexture(new Vector2D<uint>(1024, 1024), GLEnum.Rgba, GLEnum.UnsignedByte);
+            using SKPaint paint = new()
+            {
+                IsAntialias = true,
+                IsDither = true,
+                FilterQuality = SKFilterQuality.High,
+                Shader = SKShader.CreateLinearGradient(new SKPoint(begin.X * 1024, begin.Y * 1024), new SKPoint(end.X * 1024, end.Y * 1024), colors.Select(c => new SKColor(c.R, c.G, c.B, c.A)).ToArray(), null, SKShaderTileMode.Repeat)
+            };
+            surface.Canvas.DrawRect(0, 0, 1024, 1024, paint);
+
+            texture.FlushTexture(ptr, new Vector2D<uint>(1024, 1024), GLEnum.Rgba, GLEnum.UnsignedByte);
+        }
     }
 
     public static void WriteMatrixArray(this Texture2D texture, IEnumerable<Matrix4X4<float>> matrices)
     {
         Matrix4X4<float>[] matrixArray = matrices.ToArray();
 
-        texture.AllocationBuffer((uint)(matrixArray.Length * 16 * sizeof(float)), out void* pboData);
+        byte[] bytes = new byte[matrixArray.Length * 16 * sizeof(float)];
 
-        Span<Matrix4X4<float>> span = new(pboData, matrixArray.Length);
+        fixed (byte* ptr = bytes)
+        {
+            Span<Matrix4X4<float>> span = new(ptr, matrixArray.Length);
 
-        matrixArray.CopyTo(span);
+            matrixArray.CopyTo(span);
 
-        texture.FlushTexture(new Vector2D<uint>(4, (uint)matrixArray.Length), GLEnum.Rgba, GLEnum.Float);
+            texture.FlushTexture(ptr, new Vector2D<uint>(4, (uint)matrixArray.Length), GLEnum.Rgba, GLEnum.Float);
+        }
     }
 
     public static void WriteColor(this Texture2D texture, Color color)
@@ -89,12 +85,15 @@ public static unsafe class TextureExtensions
 
     public static void WriteColor(this Texture2D texture, Vector4D<byte> color)
     {
-        texture.AllocationBuffer(4, out void* pboData);
+        byte[] bytes = new byte[4];
 
-        Span<Vector4D<byte>> span = new(pboData, 1);
+        fixed (byte* ptr = bytes)
+        {
+            Span<Vector4D<byte>> span = new(ptr, 1);
 
-        span[0] = color;
+            span[0] = color;
 
-        texture.FlushTexture(new Vector2D<uint>(1, 1), GLEnum.Rgba, GLEnum.UnsignedByte);
+            texture.FlushTexture(ptr, new Vector2D<uint>(1, 1), GLEnum.Rgba, GLEnum.UnsignedByte);
+        }
     }
 }
--- SabaViewer/Tools/Texture2D.cs
+++ SabaViewer/Tools/Texture2D.cs
@@ -10,8 +10,6 @@ public unsafe class Texture2D : IDisposable
 
     public uint Id { get; }
 
-    public uint Pbo { get; }
-
     public bool HasAlpha { get; private set; } = true;
 
     public Texture2D(GL gl, GLEnum wrapParam = GLEnum.Repeat)
@@ -19,14 +17,13 @@ public unsafe class Texture2D : IDisposable
         _gl = gl;
 
         Id = _gl.GenTexture();
-        Pbo = _gl.GenBuffer();
 
         _gl.GetFloat((GLEnum)EXT.MaxTextureMaxAnisotropyExt, out float maxAnisotropy);
 
         _gl.BindTexture(GLEnum.Texture2D, Id);
 
         _gl.TexParameter(GLEnum.Texture2D, (GLEnum)EXT.MaxTextureMaxAnisotropyExt, maxAnisotropy);
-        _gl.TexParameter(GLEnum.Texture2D, GLEnum.TextureMinFilter, (int)GLEnum.Linear);
+        _gl.TexParameter(GLEnum.Texture2D, GLEnum.TextureMinFilter, (int)GLEnum.LinearMipmapLinear);
         _gl.TexParameter(GLEnum.Texture2D, GLEnum.TextureMagFilter, (int)GLEnum.Linear);
         _gl.TexParameter(GLEnum.Texture2D, GLEnum.TextureWrapS, (int)wrapParam);
         _gl.TexParameter(GLEnum.Texture2D, GLEnum.TextureWrapT, (int)wrapParam);
@@ -36,36 +33,21 @@ public unsafe class Texture2D : IDisposable
         _gl.BindTexture(GLEnum.Texture2D, 0);
     }
 
-    public void AllocationBuffer(uint pboSize, out void* pboData, bool needRead = false)
-    {
-        _gl.BindBuffer(GLEnum.PixelUnpackBuffer, Pbo);
-
-        _gl.BufferData(GLEnum.PixelUnpackBuffer, pboSize, null, GLEnum.StreamDraw);
-
-        pboData = _gl.MapBufferRange(GLEnum.PixelUnpackBuffer, 0, pboSize, needRead ? (uint)(GLEnum.MapReadBit | GLEnum.MapWriteBit) : (uint)GLEnum.MapWriteBit);
-
-        _gl.BindBuffer(GLEnum.PixelUnpackBuffer, 0);
-    }
-
-    public void FlushTexture(Vector2D<uint> size, GLEnum format, GLEnum type, bool hasAlpha = true)
+    public void FlushTexture(void* image, Vector2D<uint> size, GLEnum format, GLEnum type, bool hasAlpha = true)
     {
         HasAlpha = hasAlpha;
 
-        _gl.BindBuffer(GLEnum.PixelUnpackBuffer, Pbo);
         _gl.BindTexture(GLEnum.Texture2D, Id);
 
-        _gl.UnmapBuffer(GLEnum.PixelUnpackBuffer);
-        _gl.TexImage2D(GLEnum.Texture2D, 0, (int)format, size.X, size.Y, 0, format, type, (void*)0);
+        _gl.TexImage2D(GLEnum.Texture2D, 0, (int)format, size.X, size.Y, 0, format, type, image);
         _gl.GenerateMipmap(GLEnum.Texture2D);
 
         _gl.BindTexture(GLEnum.Texture2D, 0);
-        _gl.BindBuffer(GLEnum.PixelUnpackBuffer, 0);
     }
 
     public void Dispose()
     {
         _gl.DeleteTexture(Id);
-        _gl.DeleteBuffer(Pbo);
 
         GC.SuppressFinalize(this);
     }
